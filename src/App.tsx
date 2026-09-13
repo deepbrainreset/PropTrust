e43d7a96f3ce2236d0ce29faddda535d65a4adbb
Marcelo Simeoni <deepbrainreset@gmail.com>
2026-09-13 10:29:46 -0300

    feat: add professional directory and profile routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { HomePage } from './pages/HomePage'
 import { LoginPage } from './pages/LoginPage'
 import { OpportunitiesPage } from './pages/OpportunitiesPage'
 import { OwnerProposalsPage } from './pages/OwnerProposalsPage'
+import { ProfessionalsPage } from './pages/ProfessionalsPage'
+import { ProfessionalProfilePage } from './pages/ProfessionalProfilePage'
 import { PropertiesPage } from './pages/PropertiesPage'
 import { PropertyDetailPage } from './pages/PropertyDetailPage'
 import { PublishPropertyPage } from './pages/PublishPropertyPage'
@@ -17,6 +19,7 @@ export function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/propiedades" element={<PropertiesPage />} />
       <Route path="/propiedad/:id" element={<PropertyDetailPage />} />
+      <Route path="/profesionales" element={<ProfessionalsPage />} />
       <Route path="/ingresar" element={<LoginPage />} />
       <Route path="/registro" element={<RegisterPage />} />
       <Route path="/panel" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
@@ -24,6 +27,7 @@ export function App() {
       <Route path="/oportunidades" element={<ProtectedRoute><OpportunitiesPage /></ProtectedRoute>} />
       <Route path="/propuestas" element={<ProtectedRoute><OwnerProposalsPage /></ProtectedRoute>} />
       <Route path="/crm" element={<ProtectedRoute><CrmPage /></ProtectedRoute>} />
+      <Route path="/perfil-profesional" element={<ProtectedRoute><ProfessionalProfilePage /></ProtectedRoute>} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   </BrowserRouter>
