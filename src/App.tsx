cbdf0cee698b509bb9701d010765e0ac580fd7c6
Marcelo Simeoni <deepbrainreset@gmail.com>
2026-09-13 10:11:15 -0300

    feat: expose property catalog route

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { ProtectedRoute } from './auth/ProtectedRoute'
 import { DashboardPage } from './pages/DashboardPage'
 import { HomePage } from './pages/HomePage'
 import { LoginPage } from './pages/LoginPage'
+import { PropertiesPage } from './pages/PropertiesPage'
 import { PublishPropertyPage } from './pages/PublishPropertyPage'
 
 export function App() {
   return <BrowserRouter>
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/propiedades" element={<PropertiesPage />} />
       <Route path="/ingresar" element={<LoginPage />} />
       <Route path="/panel" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
       <Route path="/publicar" element={<ProtectedRoute><PublishPropertyPage /></ProtectedRoute>} />
