d85db31f1079dcc9b8d0b4e79cca84eb64fd91ea
Marcelo Simeoni <deepbrainreset@gmail.com>
2026-09-13 10:24:34 -0300

    feat: add property detail and CRM routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { ProtectedRoute } from './auth/ProtectedRoute'
+import { CrmPage } from './pages/CrmPage'
 import { DashboardPage } from './pages/DashboardPage'
 import { HomePage } from './pages/HomePage'
 import { LoginPage } from './pages/LoginPage'
 import { OpportunitiesPage } from './pages/OpportunitiesPage'
 import { OwnerProposalsPage } from './pages/OwnerProposalsPage'
 import { PropertiesPage } from './pages/PropertiesPage'
+import { PropertyDetailPage } from './pages/PropertyDetailPage'
 import { PublishPropertyPage } from './pages/PublishPropertyPage'
 import { RegisterPage } from './pages/RegisterPage'
 
@@ -14,12 +16,14 @@ export function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/propiedades" element={<PropertiesPage />} />
+      <Route path="/propiedad/:id" element={<PropertyDetailPage />} />
       <Route path="/ingresar" element={<LoginPage />} />
       <Route path="/registro" element={<RegisterPage />} />
       <Route path="/panel" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
       <Route path="/publicar" element={<ProtectedRoute><PublishPropertyPage /></ProtectedRoute>} />
       <Route path="/oportunidades" element={<ProtectedRoute><OpportunitiesPage /></ProtectedRoute>} />
       <Route path="/propuestas" element={<ProtectedRoute><OwnerProposalsPage /></ProtectedRoute>} />
+      <Route path="/crm" element={<ProtectedRoute><CrmPage /></ProtectedRoute>} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   </BrowserRouter>
